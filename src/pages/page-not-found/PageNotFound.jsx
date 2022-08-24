2005d53c4bcfbb22666ee134aba2441645f70b86
mdarif6 <md1arif015@gmail.com>
2022-08-24 19:26:36 +0530

    redirect file and page not found
    redirect file and page not found

---
+++ src/pages/page-not-found/PageNotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PageNotFoundMain from "./PageNotFoundMain";
+
+export default function PageNotFound() {
+  return (
+    <div>
+      <PageNotFoundMain />
+    </div>
+  );
+}
